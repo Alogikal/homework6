f6d5ad6ed24757cfec5b99d784e1c3db15210bea
Alogikal <andi.alogikal@mail.com>
2024-09-27 23:13:04 +0300

    Сдал ДЗ6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Задача 1");
         System.out.println("--------------------------");
         for ( int i = 1; i <=10; i++ ) {
